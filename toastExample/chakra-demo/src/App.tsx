6accf62a536f617e9ebcbd70d94b4b87558a6e61
mohdlateef <lateefahbaba123@gmail.com>
2025-01-23 16:10:05 +0530

    chakra dialog implimented

---
+++ toastExample/chakra-demo/src/App.tsx
@@ -0,0 +1,13 @@
+
+import { Provider } from "./components/ui/provider"
+import { Dialoge } from "./view/Dialoge"
+
+const App = () => {
+  return <>
+    <Provider>
+      <Dialoge />
+    </Provider>
+  </>
+}
+
+export default App
---
+++ toastExample/chakra-demo/src/view/Dialoge.tsx
@@ -0,0 +1,54 @@
+// import { Button } from "/components/ui/button"
+import {
+    DialogActionTrigger,
+    DialogBody,
+    DialogCloseTrigger,
+    DialogContent,
+    DialogFooter,
+    DialogHeader,
+    DialogRoot,
+    DialogTitle,
+    DialogTrigger,
+} from "../components/ui/dialog"
+import { Button } from "../components/ui/button"
+
+export const Dialoge = () => {
+    function handleCall() {
+        console.log("hello");
+    }
+    return (
+        <>
+            <h1>hey from dialog</h1>
+            <DialogRoot>
+                <DialogTrigger asChild>
+                    <Button variant="outline" size="sm">
+                        Open Dialog
+                    </Button>
+                </DialogTrigger>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>Dialog Title</DialogTitle>
+                    </DialogHeader>
+                    <DialogBody>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                    </DialogBody>
+                    <DialogFooter>
+                        <DialogActionTrigger asChild>
+                            <Button variant="outline">Cancel</Button>
+                        </DialogActionTrigger>
+                        <DialogActionTrigger>
+
+                            <Button onClick={() => { handleCall(); }}>Save</Button>
+                        </DialogActionTrigger>
+
+                    </DialogFooter>
+                    <DialogCloseTrigger />
+                </DialogContent>
+            </DialogRoot>
+        </>
+
+    )
+}
